7cd177bead27c3616652457b1bc49123e8776623
Ivan Makhambetov <i.makhambetov@gmail.com>
2017-05-21 19:10:23 +0600

    first commit

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -10,6 +10,24 @@ namespace Sum
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вычисление суммы чисел. Для выхода - \"0\"");
+            int sum = 0;
+            int n;
+            do
+            {
+                //int n = 1;
+                Console.Write("Введите число -> ");
+                string str = Console.ReadLine();
+                bool res = Int32.TryParse(str, out n);
+                if (!res){
+                    n = 1;
+                    continue;
+                }
+                sum += n;
+            } while (n != 0);
+            Console.WriteLine("Сумма: " + sum);
+
+            Console.ReadKey();
         }
     }
 }
